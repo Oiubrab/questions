6a75963075ef8c3812829cc87356f104d4d06840
Oiubrab <lz4_8fs3@protonmail.com>
2024-06-30 21:14:53 +1000

    added an inputter sub to pass down data to brain

--- inputter_module.f90
+++ inputter_module.f90
@@ -22,4 +22,20 @@ module inputter_module
         end do
     end subroutine initialize_inputter
 
+    subroutine copy_non_low_to_brain_top_row(inputter, brain, offset, inputter_len, brain_cols)
+        type(trinary), allocatable :: inputter(:)
+        type(trinary), allocatable :: brain(:,:)
+        integer, intent(in) :: offset, inputter_len, brain_cols
+        integer :: j
+
+        ! Copy non-low states from inputter to the top row of the brain matrix
+        do j = 1, inputter_len
+            if (inputter(j)%get() /= low .and. (offset + j) <= brain_cols) then
+                if (brain(1, offset + j)%get() < inputter(j)%get()) then
+                    call brain(1, offset + j)%set(inputter(j)%get())
+                end if
+            end if
+        end do
+    end subroutine copy_non_low_to_brain_top_row
+
 end module inputter_module
